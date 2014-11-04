dd76137a55ef59691f405f9dd152ac1add57ff06
Brandon Inman <brinman2002@yahoo.com>
2014-11-03 23:11:39 -0600

    formatting and comment about common code

--- plot1.R
+++ plot1.R
@@ -1,21 +1,28 @@
 
+# Begin of common code.  I'd like to place these in a separate R file and source
+# it, but it isn't clear from the project rubric that this is allowed, so I am
+# manually duplicating it across all files.  Normally this is not something I
+# would encourage.
+
 setwd("~/coursera/exdata/ExData_Plotting1")
 if(!file.exists("household_power_consumption.txt")){
- download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+   download.file(
+         "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
          destfile="power_consumption.zip")       
- unzip("power_consumption.zip" )
+   unzip("power_consumption.zip" )
 }
+
 raw_data <-subset(read.csv("household_power_consumption.txt",
         sep = ";",na.strings="?"), 
         Date == "1/2/2007" | Date == "2/2/2007")
-
-# The following is not needed for all plots but is included in each to ease
-# dealing with the submission requirements of the project.
 
 dates <- as.Date(
         strptime(paste(raw_data$Date, raw_data$Time), format= "%d/%m/%Y %H:%M:%S")
 )
 data <- cbind(raw_data,data.frame(FullDate = dates))
+
+# End of common code
+
 png(filename="plot1.png")
 
 hist(raw_data$Global_active_power, main = "Global Active Power",
